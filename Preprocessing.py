d51b6ba4bf78b93c4a175ff96093e6d440c78f26
Ayush Patel <ayushpatel0506@gmail.com>
2019-01-02 08:24:22 +0000

    completed task1

--- Preprocessing.py
+++ Preprocessing.py
@@ -14,7 +14,7 @@ import cv2
 import dlib
 img_dir='floyd\input\dataset\\'
 from lab2_landmarks import run_dlib_shape
-
+import pandas as pd
 
 
 # In[7]:
@@ -52,7 +52,8 @@ def preprocessRGB(attribute1,testsize,pictureshape):#testsize=percentage of test
         x_test[i,:,:,:]=image.img_to_array(temp)
         i=i+1
     x_train,x_test = x_train/255,x_test/255
-    return x_train,y_train,x_test,y_test
+    x_testfiles=test['file_name']
+    return x_testfiles,x_train,y_train,x_test,y_test
 
 
 def facefeaturesdlib(attribute1,testsize,pictureshape):
@@ -85,6 +86,30 @@ def facefeaturesdlib(attribute1,testsize,pictureshape):
         img=image.img_to_array(img)
         x_test[i,:,:], _=run_dlib_shape(img)
         i=i+1
+    x_test=np.reshape(x_test,(x_test.shape[0],x_test.shape[1]*2),'f')
+    df = pd.DataFrame(x_test)
+    df['y']=y_test
+    fdf=df.dropna(axis=0)
+    t=fdf.shape[0]
+    x_test=fdf.iloc[:,:-1]
+    y_test=fdf['y']
+    x_test=np.array(x_test)
+    y_test=np.array(y_test)
+    x_test=np.reshape(x_test,(x_test.shape[0],68,2),'f')
+    
+    x_train=np.reshape(x_train,(x_train.shape[0],x_train.shape[1]*2),'f')
+    df = pd.DataFrame(x_train)
+    df['y']=y_train
+    fdf=df.dropna(axis=0)
+    t=fdf.shape[0]
+    x_train=fdf.iloc[:,:-1]
+    y_train=fdf['y']
+    x_train=np.array(x_train)
+    y_train=np.array(y_train)
+    x_train=np.reshape(x_train,(x_train.shape[0],68,2),'f')
+    
+    
+    
     return x_train,y_train,x_test,y_test
 
 
---
+++ README.txt
@@ -0,0 +1,6 @@
+
+
+____________________________________________________________________________
+RUN remove_noise.py to get noise_classified.csv(contains names of files 
+and whether its noise(0) or a face (1))
+____________________________________________________________________________
